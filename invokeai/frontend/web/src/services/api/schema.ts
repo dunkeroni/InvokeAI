15e45b3367f29392b9378b275cffd4f28d33a3ea
dunkeroni <dunkeroni@gmail.com>
2025-05-03 00:51:00 -0400

    compositing in frontend

--- invokeai/frontend/web/src/services/api/schema.ts
+++ invokeai/frontend/web/src/services/api/schema.ts
@@ -10236,6 +10236,11 @@ export type components = {
              */
             image?: components["schemas"]["ImageField"];
             /**
+             * @description Optional mask determining where to apply noise (black=noise, white=no noise)
+             * @default null
+             */
+            mask?: components["schemas"]["ImageField"] | null;
+            /**
              * Seed
              * @description Seed for random number generation
              * @default 0
